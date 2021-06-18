61ae671ea7deea9f0c23b415fcb3e2be0486b6cd
kotaro-ktr <83695177+kotaro-ktr@users.noreply.github.com>
2021-06-18 22:23:04 +0900

    Revert "Merge pull request #9 from kotaro-ktr/second_training"
    This reverts commit 813337177a7c86ee9b28273e4178a7863f8689f0, reversing
    changes made to c1b34aea9a495698243b2667ba250155787a2b6e.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:plan).permit(:date, :plan)
+    params.require(:calendars).permit(:date, :plan)
   end
 
   def get_week
@@ -34,13 +34,7 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-
-      wdays_num = @todays_date.wday + x
-      if 7 <= wdays_num
-        wdays_num = wdays_num - 7
-      end
-
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[wdays_num]}
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
       @week_days.push(days)
     end
 
